5c53c27f83d6baa9b43beebbcadc1fb6d007404c
PC1-IFLAB <novitasabila70@gmail.com>
2023-03-20 07:58:21 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- mod5_1302213030/Penjumlahan.cs
+++ mod5_1302213030/Penjumlahan.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302213030
+{
+    class Penjumlahan<T>
+    {
+        public void JumlahTigaAngka(T x1, T x2, T x3)
+        {
+            dynamic dyx1 = x1;
+            dynamic dyx2 = x2;
+            dynamic dyx3 = x3;
+
+            T hasil = dyx1 + dyx2 + dyx3;
+            Console.WriteLine(hasil);
+        }
+        a
+    }
+}
--- mod5_1302213030/Program.cs
+++ mod5_1302213030/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302213030
+{
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Penjumlahan<long> jumlah = new Penjumlahan<long>();
+
+            jumlah.JumlahTigaAngka(13, 02, 21);
+        }
+    }
+}
