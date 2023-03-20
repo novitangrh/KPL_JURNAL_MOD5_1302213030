f25421b5fd05a6c26c92aafbaa882428c900d7a2
PC1-IFLAB <novitasabila70@gmail.com>
2023-03-20 08:27:43 +0700

    menambahkan class SimpleDataBase

--- mod5_1302213030/Program.cs
+++ mod5_1302213030/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302213030
+{
+
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+         
+        }
+    }
+}
---
+++ mod5_1302213030/SimpleDataBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod5_1302213030
+{
+    internal class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            List<T> data = new List<T>();
+        }
+
+        public void AddNewData(T data)
+        {
+            this.storedData.Add(data);
+            this.inputDates.Add(DateTime.Now);
+        }
+      
+        public void PrintAllData()
+        {
+            for(int i = 0;i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data" + i + " berisi "
+                    + storedData + " , yang disimpan pada waktu UTC: "
+                    + inputDates);
+            }
+            
+        }
+
+
+
+    }
+}
